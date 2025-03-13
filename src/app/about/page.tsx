a78059c3a64c48f1e224deeeec252285ca042c9f
Erika <erika@7011.local>
2025-03-13 12:23:33 -0600

    Correción de archivos

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div className="p-4">
+        <h1 className="text-3xl font-bold underline">
+          Acerca de nosotros
+        </h1>
+        <p className="mt-4">Esta es la página de acerca de.</p>
+      </div>
+    );
+  }
