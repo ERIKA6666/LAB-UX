389c7c1b8c939828576720a8add307305bef0a62
caravanadestrucs <dariocasoca@gmail.com>
2025-06-05 14:46:05 -0600

    redireccion por no estar loggin

--- src/app/layoutSwitcher.tsx
+++ src/app/layoutSwitcher.tsx
@@ -1,25 +1,28 @@
 'use client'
 import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import LayoutNormal from "./layoutNormal";
 import LayoutAdmin from "./layoutAdmin";
 import  "../styles/globals.css"; // Asegúrate de importar tus estilos globales
-
 import { getUsuarioActual, cerrarSesion } from "../hooks/islogin"; // Ajusta según tu auth
 
 interface LayoutProps {
   children: React.ReactNode; // Define el tipo de children
 }
 
-
 const LayoutSwitcher = ({ children }: LayoutProps) => {
   const [usuario, setUsuario] = useState(null);
-  const pathname : string | null = usePathname() ?? "/";
+  const pathname: string = usePathname() ?? "/";
+  const router = useRouter();
 
   useEffect(() => {
     const user = getUsuarioActual();
     setUsuario(user);
-  }, []);
+    // Si está en admin y no hay usuario, redirige a /
+    if (pathname.startsWith("/admin") && !user) {
+      router.replace("/");
+    }
+  }, [pathname]);
 
   const handleLogout = () => {
     cerrarSesion();
@@ -31,7 +34,10 @@ const LayoutSwitcher = ({ children }: LayoutProps) => {
   if (usuario && pathname.startsWith("/admin")) {
     return <LayoutAdmin>{children}</LayoutAdmin>;
   }
-
+  // Si es una ruta de admin y el usuario está logueado, usa LayoutAdmin
+  if (pathname.startsWith("/admin")) {
+    return <LayoutAdmin>{children}</LayoutAdmin>;
+  }
   // En cualquier otro caso, usa LayoutNormal
   return <LayoutNormal>{children}</LayoutNormal>;
 };
