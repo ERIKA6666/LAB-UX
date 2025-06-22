ff9d62e9e579c8f8688ab7a198781267a3a41271
Erika <li_erikacs@unca.edu.mx>
2025-06-22 16:55:07 -0600

    endpoints usuarios

---
+++ src/config/endpoints.ts
@@ -0,0 +1,22 @@
+import { API_URL } from "@/constans/Api";
+
+export const ENDPOINTS = {
+    USUARIOS : {
+      BASE: `${API_URL}/usuarios`,
+      BY_ID : (id:number) => `${API_URL}/usuarios/${id}`,
+      LOGIN: `${API_URL}/usuarios/login`,
+      RESET_PASSWORD: `${API_URL}/usuarios/reset_password`,
+      AVATAR: (filename: string) => `${API_URL}/uploads/${filename}`,
+      SEARCH: (params: {
+        search?: string;
+        rol?: string;
+        estado?: string;
+        }) => {
+        const urlParams = new URLSearchParams();
+        if (params.search) urlParams.append("q", params.search);
+        if (params.rol && params.rol !== "todos") urlParams.append("tipo_usuario", params.rol);
+        if (params.estado && params.estado !== "todos-status") urlParams.append("estado", params.estado);
+        return `${API_URL}/usuarios?${urlParams.toString()}`;
+        },
+    }
+}
